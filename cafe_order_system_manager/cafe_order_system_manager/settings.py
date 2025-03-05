81108c06f337e1d674c2f7c07553619ed8694108
Ustinov <test4ndworkustinoff@gmail.com>
2025-03-05 14:40:26 +0300

    Добавлен css, переписан html

--- cafe_order_system_manager/cafe_order_system_manager/settings.py
+++ cafe_order_system_manager/cafe_order_system_manager/settings.py
@@ -1,64 +1,54 @@
 import os
 
-# Определяем базовую директорию проекта. Это полезно для построения путей к другим файлам и директориям.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Секретный ключ для проекта. Он используется для шифрования данных. В реальном проекте его следует хранить в переменной окружения.
 SECRET_KEY = 'your-secret-key'
 
-# Режим отладки. Включает подробные сообщения об ошибках и другие функции, полезные при разработке.
 DEBUG = True
 
-# Список разрешенных хостов. В режиме отладки обычно пустой, но в продакшене должен содержать домены, с которых разрешено обращаться к приложению.
 ALLOWED_HOSTS = []
 
-# Список установленных приложений. Включает как встроенные приложения Django, так и пользовательские.
 INSTALLED_APPS = [
-    'django.contrib.admin',         # Административный интерфейс
-    'django.contrib.auth',          # Система аутентификации
-    'django.contrib.contenttypes',  # Работа с типами контента
-    'django.contrib.sessions',      # Поддержка сессий
-    'django.contrib.messages',      # Сообщения для пользователей
-    'django.contrib.staticfiles',   # Обработка статических файлов
-    'orders',                       # Пользовательское приложение для управления заказами
-    'rest_framework',               # Django REST Framework для создания API
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'orders',
+    'rest_framework',
 ]
 
-# Список промежуточного ПО (middleware), которое обрабатывает запросы.
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',          # Обеспечение безопасности
-    'django.contrib.sessions.middleware.SessionMiddleware',  # Поддержка сессий
-    'django.middleware.common.CommonMiddleware',             # Общие задачи
-    'django.middleware.csrf.CsrfViewMiddleware',             # Защита от CSRF атак
-    'django.contrib.auth.middleware.AuthenticationMiddleware', # Аутентификация
-    'django.contrib.messages.middleware.MessageMiddleware',   # Сообщения
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',# Защита от clickjacking
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Основной файл конфигурации URL.
 ROOT_URLCONF = 'cafe_order_system_manager.urls'
 
-# Настройки шаблонов.
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',  # Используемый движок шаблонов
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Директории с шаблонами
-        'APP_DIRS': True,  # Включать ли директории приложений в поиск шаблонов
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',  # Переменные контекста для отладки
-                'django.template.context_processors.request', # Переменные контекста запроса
-                'django.contrib.auth.context_processors.auth', # Переменные контекста аутентификации
-                'django.contrib.messages.context_processors.messages', # Переменные контекста сообщений
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-# Настройка WSGI приложения.
 WSGI_APPLICATION = 'cafe_order_system_manager.wsgi.application'
 
-# Настройки базы данных. Используется SQLite по умолчанию.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -66,7 +56,6 @@ DATABASES = {
     }
 }
 
-# Валидаторы паролей.
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -82,16 +71,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Языковые настройки.
 LANGUAGE_CODE = 'ru-ru'
 
-# Часовой пояс.
 TIME_ZONE = 'UTC'
 
-# Настройки интернационализации.
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
-# URL для статических файлов.
 STATIC_URL = '/static/'
+
+# Добавьте следующие строки для настройки статических файлов
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'orders/static'),
+]
